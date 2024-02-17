e5cb0447c744c7857d29509e376da72b49208fac
yassnath <143576423+yassnath@users.noreply.github.com>
2024-02-17 18:23:51 +0700

    Sistem Bilangan
    - Converter Binary to Decimal
    - Converter Decimal to Binary
    - Converter Octal to Decimal
    - Converter Decimal to Octal
    - Converter Hexadecimal to Decimal
    - Converter Decimal to Hexadecimal

---
+++ Sistem Bilangan.py
@@ -0,0 +1,17 @@
+# bin to dec
+print(int(0b101))
+
+# dec to bin
+print(bin(5))
+
+# oct to dec
+print(int(0o101))
+
+# dec to oct
+print (oct(65))
+
+# hex to dec
+print(int(0x101))
+
+# dec to hex
+print(hex(257))
