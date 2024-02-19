3115bdcfc902bf7d344c199f0a6e2adbf485992d
yassnath <143576423+yassnath@users.noreply.github.com>
2024-02-19 21:31:22 +0700

    Delete Conditionals_Grading.py

--- Conditionals_Grading.py
+++
@@ -1,9 +0,0 @@
-nilai = int(input("Masukkan nilai: "))
-if nilai >= 90:
- print("Nilai huruf: A")
-elif nilai >= 75:
- print("Nilai huruf: B")
-elif nilai >= 55:
- print("Nilai huruf: C")
-else:
- print("Nilai huruf: D")
