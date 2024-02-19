e4b274e7a27cf7c430d9eea9cb8b6667a3388020
yassnath <143576423+yassnath@users.noreply.github.com>
2024-02-19 21:33:13 +0700

    Conditionals (Percabangan)
    - Grading
    - Password Checker
    - Recommender

---
+++ Conditionals_Recommender.py
@@ -0,0 +1,12 @@
+suka_pedas = input("Suka pedas (Y/T)? ")
+tanggal_tua = input("Tanggal tua (Y/T)? ")
+if suka_pedas == "Y":
+ if tanggal_tua == "Y":
+    print("Rekomendasi menu: Lalapan pedas")
+ else:
+    print("Rekomendasi menu: Nasi rica-rica iga sapi")
+else:
+ if tanggal_tua == "Y":
+     print("Rekomendasi menu: Nasi goreng")
+ else:
+    print("Rekomendasi menu: Nasi ayam kecap")
